d34df98403ab78b5a951de760b92ebace5f647a6
Brandon Li <48413902+aspiringLich@users.noreply.github.com>
2025-04-28 23:57:19 -0400

    map!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,50 @@
 <script lang="ts">
 	import { Button } from 'flowbite-svelte';
+	import { type LatLngTuple } from 'leaflet';
+
+	let map: L.Map | undefined;
+	let initialView: LatLngTuple = [40.716, -74.467];
+
+	async function createMap(container: HTMLElement) {
+	    const L = await import("leaflet");
+		map = L.map(container, { preferCanvas: true }).setView(initialView, 12);
+		// var searchControl = L.es/ri.Geocoding.geosearch().addTo(map);
+
+		L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+			attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
+		        &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`,
+			subdomains: 'abcd',
+			maxZoom: 14
+		}).addTo(map);
+	}
+
+	function mapAction(container: HTMLElement) {
+		createMap(container).then();
+
+		return {
+			destroy: () => {
+				map?.remove();
+				map = undefined;
+			}
+		};
+	}
 </script>
 
-<Button>
-    Hello
-</Button>
+<svelte:window
+	on:resize={() => {
+		if (map) map.invalidateSize();
+	}}
+/>
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+		integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+		crossorigin=""
+	/>
+</svelte:head>
+
+<div class="flex flex-row">
+	<div class="basis-96">hihi</div>
+	<main class="h-[100vh] grow" use:mapAction></main>
+</div>
