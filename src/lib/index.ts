06105200402a496f462000990eb20900f7086284
Brandon Li <48413902+aspiringLich@users.noreply.github.com>
2025-04-30 11:09:20 -0400

    parse google sheets 1

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,91 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+export class Data {
+	parents: Record<string, { data: { [key: string]: string }; address: number }>;
+	children: Record<string, { data: { [key: string]: string }; parents: string[]; address: number }>;
+	cars: { parents: string[]; address: number }[];
+	addresses: { lon: number; lat: number; name: string }[];
+
+	constructor() {
+		this.parents = {};
+		this.children = {};
+		this.cars = [];
+		this.addresses = [];
+	}
+
+	static fromSheetData(data: string[][], addToLog: (add: string) => void): Data | undefined {
+		const d = new Data();
+
+		// == SCAN HEADER INDECES (pass 1)
+		const headers = data[0] as string[];
+
+		const logThrow = (s: string) => {
+			addToLog(s);
+			addToLog('!Aborting...');
+			throw s;
+		};
+
+		// check for duplicates
+		const seenHeaders = new Set<string>();
+		for (const header of headers) {
+			const h = header.trim().split(' ').join(' ');
+			if (seenHeaders.has(h)) {
+				logThrow(`!Duplicate header found: ${header}`);
+			}
+			seenHeaders.add(h);
+		}
+
+		const columns = new Set<number>();
+		for (let i = 0; i < headers.length; i++) columns.add(i);
+		const rmvCol = (i: number) => {
+			if (columns.delete(i)) return i;
+			else {
+				console.log(data);
+				logThrow('!This is a bug! Same columns triggered twice, check logs\n');
+				throw 0;
+			}
+		};
+
+		const parent_idx: { [key: string]: number } = {};
+		const child_idx: { [key: string]: number } = {};
+		let passenger_capacity_idx = -1;
+		let address_idx = -1;
+
+		for (let i = 0; i < headers.length; i++) {
+			const split = headers[i].trim().split(' ');
+			if (split.length == 2 && split[0] == 'Parent') {
+				parent_idx[split[1]] = rmvCol(i);
+			} else if (split.length == 2 && split[0] == 'Passenger' && split[1] == 'Capacity') {
+				passenger_capacity_idx = rmvCol(i);
+			} else if (split.length == 2 && split[0] == 'Child') {
+				child_idx[split[1]] = rmvCol(i);
+			} else if (split.length == 1 && split[0] == 'Address') {
+				address_idx = rmvCol(i);
+			}
+		}
+		addToLog(`Found ${Object.keys(parent_idx).length} parents groups per row`);
+		addToLog(`Found ${Object.keys(child_idx).length} child groups per row`);
+
+		if (passenger_capacity_idx == -1) logThrow('!No Passenger Capacity column set');
+		if (address_idx == -1) logThrow('!No Address column set');
+
+		const parent_data_idx: { [key: string]: [string, number] } = {};
+		const child_data_idx: { [key: string]: [string, number] } = {};
+		for (let i = 0; i < headers.length; i++) {
+			const split = headers[i].trim().split(' ');
+			if (split.length >= 3 && split[0] == 'Parent') {
+				if (parent_idx[split[1]] === undefined) {
+					logThrow(`!Unknown parent column: Parent ${split[1]} referenced in column ${i + 1}`);
+				}
+				parent_data_idx[split[1]] = [split.splice(2).join(' '), rmvCol(i)];
+			} else if (split.length >= 3 && split[0] == 'Child') {
+				if (child_idx[split[1]] === undefined) {
+					logThrow(`!Unknown child column: Child ${split[1]} referenced in column ${i + 1}`);
+				}
+				child_data_idx[split[1]] = [split.splice(2).join(' '), rmvCol(i)];
+			}
+		}
+
+		return d;
+	}
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { Data } from '$lib';
 	import { Button, Modal, Label, Input } from 'flowbite-svelte';
 	import type { LatLngTuple } from 'leaflet';
 	import Papa from 'papaparse';
@@ -33,13 +34,29 @@
 	let spreadsheet_id = $state('');
 	let valid_spreadsheet_id = $derived(spreadsheet_id.length == 44);
 
+	let log = $state('');
 	$effect(() => {
 		if (!valid_spreadsheet_id) return;
+		log = '';
 
 		Papa.parse(`https://docs.google.com/spreadsheets/d/${spreadsheet_id}/export?format=csv`, {
 			download: true,
 			header: false,
-			complete: console.log
+			complete: (sheet) => {
+				log += 'Successfully fetched google sheet\n';
+				console.log(sheet);
+				const data = sheet.data as string[][];
+				log += `${data[0].length} cols, ${data.length} rows\n`;
+
+				try {
+					let d = Data.fromSheetData(data, (s) => (log += s + '\n'));
+				} catch (e) {
+					console.error(e);
+				}
+			},
+			error: () => {
+				log += '!Error fetching google sheet\n';
+			}
 		});
 	});
 </script>
@@ -69,15 +86,18 @@
 				<li><code>Parent [X]</code></li>
 				<li><code>Parent [X] [INFO]</code></li>
 				<li><code>Passenger Capacity</code></li>
-				<li><code>Child [X]</code></li>
-				<li><code>Child [X] Sex</code></li>
+				<li><code>Child [Y]</code></li>
+				<li><code>Child [Y] [INFO]</code></li>
 				<li><code>Address</code></li>
 			</ul>
 		</div>
 		<pre class="grow"><code
 				>{#if !valid_spreadsheet_id}<span class="text-red-400"
 						>Waiting for valid Spreadsheet ID...</span
-					>{:else}<span class="text-green-400">Valid Spreadsheet ID detected</span>{/if}<br /></code
+					>{:else}<span class="text-green-400">Valid Spreadsheet ID detected</span>{/if}<br
+				/>{#each log.split('\n') as line, i (i)}{#if line[0] == '!'}<span class="text-red-400"
+							>{line.slice(1)}</span
+						>{:else}{line}{/if}<br />{/each}</code
 			></pre>
 	</div>
 </Modal>
